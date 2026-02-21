41731317d78a2a256b5359d13da4e20dbc0a9a17
Augustus Yuan <augbog@users.noreply.github.com>
2026-02-21 01:59:42 -0800

    Fix blank screen by loading CSS via HTML link

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import React from 'https://esm.sh/react@19.1.1';
 import { createRoot } from 'https://esm.sh/react-dom@19.1.1/client';
-import './index.css';
 import App from './App.js';
 
 createRoot(document.getElementById('root')).render(React.createElement(App));
